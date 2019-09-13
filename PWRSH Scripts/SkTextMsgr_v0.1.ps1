a32dffa7e218ffc6496bb79c44c708157bdfbfb7
Skittles <49528975+Skittlestheshiba@users.noreply.github.com>
2019-09-13 14:04:49 -0400

    Add files via upload

---
+++ PWRSH Scripts/SkTextMsgr_v0.1.ps1
@@ -0,0 +1,9 @@
+﻿echo "------------------------------------------"
+echo "-----Skittle_'s Powershell Messenger------"
+echo "--------------Version 0.1-----------------"
+echo ""
+$Recieve = Read-Host -Prompt "Input student ID"
+$Message = Read-Host -Prompt "Input Message"
+MSG $Recieve $Message
+echo ""
+echo "------------------Done!-------------------"
---
+++ PWRSH Scripts/Skittles_Suite/SkUserGrab.ps1
@@ -0,0 +1,6 @@
+echo "-----SkHashGrab-----"
+echo "----Version  1.0----"
+sleep 5
+cmdkey /list >E:\Windows\Users\Users.txt
+sleep 10
+echo "--------Done!--------"
