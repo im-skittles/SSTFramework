90b2878a618786eabae8eeed1a8ffea569306cdd
Skittles <49528975+Skittlestheshiba@users.noreply.github.com>
2019-09-14 15:47:39 -0400

    Delete SkTextMsgr_v0.1.ps1

--- PWRSH Scripts/SkTextMsgr_v0.1.ps1
+++
@@ -1,9 +0,0 @@
-﻿echo "------------------------------------------"
-echo "-----Skittle_'s Powershell Messenger------"
-echo "--------------Version 0.1-----------------"
-echo ""
-$Recieve = Read-Host -Prompt "Input student ID"
-$Message = Read-Host -Prompt "Input Message"
-MSG $Recieve $Message
-echo ""
-echo "------------------Done!-------------------"
